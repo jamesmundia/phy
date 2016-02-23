c7edfabf7ddd6c842a0b56e8a8d8d8d88d493e7b
James Mundia <jamesmundia@gmail.com>
2016-02-23 07:46:05 -0500

    Basic Routing, Template Setup
    Changed single injury to injury listing template to prevent confusion.
    set up the basis for a single post route

--- client/templates/injuries/injury_list.js
+++ client/templates/injuries/injury_list.js
@@ -1,5 +1,16 @@
 Template.injuryList.helpers({
   injuries: function () {
   	return Injuries.find();
-  }
+  },
+  //injury post URL generation
+  pathForPost: function () {
+  	var post = this;
+  	var params = {
+  		postId: post._id
+  	};
+  	var routeName = "singleInjury";
+  	var path = FlowRouter.path(routeName, params);
+  		return path;
+  	}
 });
+
