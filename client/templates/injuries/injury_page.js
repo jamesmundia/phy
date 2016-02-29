fcfe40dd85253cac9744f4c6711769b133d451ef
James Mundia <jamesmundia@gmail.com>
2016-02-28 23:03:54 -0500

    still working with data context, adding new branch for this work

--- client/templates/injuries/injury_page.js
+++ client/templates/injuries/injury_page.js
@@ -1,15 +1,19 @@
-Template.injuryPage.onCreated(function(){
+/*Template.injuryPage.onCreated(function(){
 	var self = this;
 	self.autorun(function(){
 	var id = FlowRouter.getParam('id');
 		self.subscribe('injuryPage', id);
 	}); // prevents subsription to previous items, performative change here - LUT
 });
+*/
 
 Template.injuryPage.helpers({
-	injury: ()=> {
+	/*injury: ()=> {
 		var id = FlowRouter.getParam('id')
 	  	return Injuries.findOne({_id: id});
-	 }
-}); 
-
+	 },*/
+	  player: function (){
+	  	var id = FlowRouter.getParam('id')
+	  	return Injuries.findOne(id);
+	  }
+});
