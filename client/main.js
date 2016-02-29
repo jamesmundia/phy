a9d0ec68a2892bc27233ea0695b10683dbe49a1e
James Mundia <jamesmundia@gmail.com>
2016-02-29 11:16:56 -0500

    Rehab schema added, insert and update forms added
    need to take blank rehab entry of of basic insert form
    need to sort out basic auto form settings, including global template
    helpers, to make form functional and to appear correctly

--- client/main.js
+++ client/main.js
@@ -1,1 +1,12 @@
 Meteor.subscribe('injuries');
+
+Template.registerHelper("intervalRunOptions", function() {
+    return [
+        {label: "Interval Run 1A", value: Run1A},
+        {label: "Interval Run 1B", value: Run1B},
+        {label: "Interval Run 1C", value: Run1C},
+        {label: "Interval Run 2A", value: Run2A},
+        {label: "Interval Run 2B", value: Run2B},
+        {label: "Interval Run 2C", value: Run2C},
+    ];
+});
