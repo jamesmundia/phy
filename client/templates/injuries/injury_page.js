f9fe03f11be2c3c0a5b6d9123efb1c0ed48eae18
James Mundia <jamesmundia@gmail.com>
2016-02-28 23:40:21 -0500

    Data context fixed, template updated
    Need to use injury.parameter to pull that from the template helper,
    which was done on a previous version of the app.
    updated fixtures and template to show more injury data like date and
    notes.
    next need to created edit, upsert functions and also attach the rehab
    schema to select interval running exercises for each injury through a
    date picker / auto form

--- client/templates/injuries/injury_page.js
+++ client/templates/injuries/injury_page.js
@@ -1,19 +1,16 @@
-/*Template.injuryPage.onCreated(function(){
+// prevents subsription to previous items, performative change here - LUT
+
+Template.injuryPage.onCreated(function(){
 	var self = this;
 	self.autorun(function(){
 	var id = FlowRouter.getParam('id');
 		self.subscribe('injuryPage', id);
-	}); // prevents subsription to previous items, performative change here - LUT
+	}); 
 });
-*/
 
 Template.injuryPage.helpers({
-	/*injury: ()=> {
+	injury: ()=> {
 		var id = FlowRouter.getParam('id')
 	  	return Injuries.findOne({_id: id});
-	 },*/
-	  player: function (){
-	  	var id = FlowRouter.getParam('id')
-	  	return Injuries.findOne(id);
-	  }
+	 }
 });
