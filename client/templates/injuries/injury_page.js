a751c092b4f4c8e297eb173be04d504fed42e681
James Mundia <jamesmundia@gmail.com>
2016-02-29 13:34:14 -0500

    Rehab Features Added
    Changed Date input to a date picket with quick form on injury add and
    rehab exercise add
    Wired to live rehab schema
    modified schema to fit needs
    created custom auto form

--- client/templates/injuries/injury_page.js
+++ client/templates/injuries/injury_page.js
@@ -12,5 +12,25 @@ Template.injuryPage.helpers({
 	injury: ()=> {
 		var id = FlowRouter.getParam('id')
 	  	return Injuries.findOne({_id: id});
-	 }
-});
+	 },
+	options: function () {
+    return [
+      {
+        optgroup: "Big Intervals",
+        options: [
+          {label: "Interval Run 1A", value: 'Interval Run 1A'},
+          {label: "Interval Run 1B", value: 'Interval Run 1B'},
+          {label: "Interval Run 1C", value: 'Interval Run 1C'}
+        ]
+      },
+      {
+        optgroup: "Medium Intervals",
+        options: [
+          {label: "Interval Run 2A", value: 'Interval Run 2A'},
+          {label: "Interval Run 2B", value: 'Interval Run 2B'},
+          {label: "Interval Run 2C", value: 'Interval Run 1C'}
+        ]
+      }
+    ];
+  }
+}); // end of HELPER
