186108ca1c2bf546eeed04aa1e831f08fb538ba6
James Mundia <jamesmundia@gmail.com>
2016-03-02 09:07:48 -0500

    3/2 Edits
    -Fiddled with the header
    -Added a submit injury page and -added that to the header
    -removed insecure
    -changed layout to have top, middle, bottom, footer
    -paper wireframe
    setting framework for rehab listing templates (no helpers - this needs
    to be written out in the same way injuries were to avoid confusion

---
+++ client/helpers/schema_helpers.js
@@ -0,0 +1,32 @@
+/* Template.injuryPage.helpers({
+  getInjury: function () {
+    var injuryId = FlowRouter.getParam('id');
+    var inj = Injuries.findOne(injuryId) || {};
+    return inj;
+  },
+	injury: ()=> {
+		var id = FlowRouter.getParam('id')
+	  	return Injuries.findOne({_id: id});
+	 },
+	options: function () {
+    return [
+      {
+        optgroup: "Big Intervals",
+        options: [
+          {label: "Interval Run 1A", value: 'Interval Run 1A'},
+          {label: "Interval Run 1B", value: 'Interval Run 1B'},
+          {label: "Interval Run 1C", value: 'Interval Run 1C'}
+        ]
+      },
+      {
+        optgroup: "Medium Intervals",
+        options: [
+          {label: "Interval Run 2A", value: 'Interval Run 2A'},
+          {label: "Interval Run 2B", value: 'Interval Run 2B'},
+          {label: "Interval Run 2C", value: 'Interval Run 1C'}
+        ]
+      }
+    ];
+  }
+}); */
+
